d60bd0707fa6c3986b4deac8a5880917f7e423b1
manahanCoded <149740716+manahanCoded@users.noreply.github.com>
2024-01-07 16:27:56 +0800

    Add files via upload

--- react.js
+++ react.js
@@ -1,1 +1,36 @@
+let available_keywords = ["Juice wrld", "My Eyes","She Knows", "Capo", 
+                            "9 AM", "All for Us", "Creepin", "SDP", "I Know"];
+
+
+const resultBox = document.querySelector(".result_box");
+const inputBox = document.querySelector("#input_box");
+
+inputBox.onkeyup = function(){
+  let result = [];
+  let input = inputBox.value;
+  if(input.length){
+    result = available_keywords.filter((keyword)=>{
+     return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    
+  }
+  display(result);
+
+  if(!result.length){
+    resultBox.innerHTML ='';
+  }
+}
+
+function display(result){
+  const content = result.map((list)=>{
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  });
+  resultBox.innerHTML = "<ul>"+ content.join('') +"</ul>"
+}
+
+function selectInput(list){
+  inputBox.value = list.innerHTML;
+  resultBox.innerHTML = '';
+}
+
 
